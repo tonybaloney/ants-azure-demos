29ba940faf4e422e39a34043cbab738c10194922
Anthony Shaw <anthony.p.shaw@gmail.com>
2021-02-09 13:00:31 +1100

    use a bytestream

--- python-functions/BlobTrigger/__init__.py
+++ python-functions/BlobTrigger/__init__.py
@@ -1,9 +1,25 @@
 import logging
-
+import io
+import os
+from azure.cosmosdb.table.tableservice import TableService
 import azure.functions as func
+import pandas
 
 
 def main(myblob: func.InputStream):
-    logging.info(f"Python blob trigger function processed blob \n"
-                 f"Name: {myblob.name}\n"
-                 f"Blob Size: {myblob.length} bytes")
+    logging.info(
+        f"Python blob trigger function processed blob \n"
+        f"Name: {myblob.name}\n"
+        f"Blob Size: {myblob.length} bytes"
+    )
+    fs = io.BytesIO()
+    myblob.readinto(fs.getbuffer())
+    df = pandas.read_excel(fs, index_col=0, engine="openpyxl")
+
+    table_service = TableService(
+        account_name=os.getenv("TABLE_STORAGE_ACCOUNT_NAME"),
+        account_key=os.getenv("TABLE_STORAGE_KEY"),
+    )
+    records = df.to_dict(orient="records")
+    for record in records:
+        table_service.insert_entity("ais", record)
