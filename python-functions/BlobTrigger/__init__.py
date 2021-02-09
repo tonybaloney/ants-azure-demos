d5be8ce60ec17796b8dfea8591023de93272b4f3
Anthony Shaw <anthony.p.shaw@gmail.com>
2021-02-09 14:20:04 +1100

    Upload rows in a big batch

--- python-functions/BlobTrigger/__init__.py
+++ python-functions/BlobTrigger/__init__.py
@@ -2,6 +2,8 @@ import logging
 import io
 import os
 from azure.cosmosdb.table.tableservice import TableService
+from azure.cosmosdb.table.tablebatch import TableBatch
+
 import azure.functions as func
 import pandas
 
@@ -12,14 +14,22 @@ def main(myblob: func.InputStream):
         f"Name: {myblob.name}\n"
         f"Blob Size: {myblob.length} bytes"
     )
-    fs = io.BytesIO()
-    myblob.readinto(fs.getbuffer())
-    df = pandas.read_excel(fs, index_col=0, engine="openpyxl")
+    df = pandas.read_excel(
+        io.BytesIO(myblob.read()),
+        engine="openpyxl",
+        index_col=0,
+        true_values=("Y",),
+        false_values=("n",),
+    )
 
     table_service = TableService(
         account_name=os.getenv("TABLE_STORAGE_ACCOUNT_NAME"),
         account_key=os.getenv("TABLE_STORAGE_KEY"),
     )
     records = df.to_dict(orient="records")
+    batch = TableBatch()
     for record in records:
-        table_service.insert_entity("ais", record)
+        entry = record | {"PartitionKey": myblob.name, "RowKey": record["ABN"]}
+        batch.insert_entity(entry)
+
+    table_service.commit_batch("ACN", batch)
