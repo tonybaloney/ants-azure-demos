8f95bd5e37439f1961565d0f16b8854c14fdab6a
Anthony Shaw <anthony.p.shaw@gmail.com>
2021-02-10 14:32:55 +1100

    camel case property names

--- python-functions/BlobTrigger/__init__.py
+++ python-functions/BlobTrigger/__init__.py
@@ -18,17 +18,30 @@ def main(myblob: func.InputStream):
         io.BytesIO(myblob.read()),
         engine="openpyxl",
         true_values=("Y",),
-        false_values=("n",),
+        false_values=("n", "", " ", None),
     )
 
     table_service = TableService(
         account_name=os.getenv("TABLE_STORAGE_ACCOUNT_NAME"),
         account_key=os.getenv("TABLE_STORAGE_KEY"),
     )
-    records = df.to_dict(orient="records")
-    batch = TableBatch()
-    for record in records:
-        entry = record | {"PartitionKey": f"ais-{myblob.name}", "RowKey": record["ABN"]}
-        batch.insert_entity(entry)
 
-    table_service.commit_batch("ACN", batch)
+    records = df.to_dict(orient="records")
+
+    # Batch in 100 rows
+    for record_batch in (
+        records[pos : pos + 100] for pos in range(0, len(records), 100)
+    ):
+        batch = TableBatch()
+        for record in record_batch:
+            entry = {
+                "PartitionKey": f"ais",
+                "RowKey": str(record["ABN"]),
+            }
+            for k, v in record.items():
+                # Create a friendly camel-case key, e.g. Shoe_MAKER___Fox becomes ShoeMakerFox
+                entry[k.replace("_", " ").title().replace(" ", "")] = v
+
+            batch.insert_entity(entry)
+
+        table_service.commit_batch("ACN", batch)
