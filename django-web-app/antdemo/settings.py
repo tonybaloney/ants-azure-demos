0ab19afbab19721e6c9a0ad8c89dd310dcdbe335
Anthony Shaw <anthony.p.shaw@gmail.com>
2021-03-08 14:59:25 +1100

    add psycho

--- django-web-app/antdemo/settings.py
+++ django-web-app/antdemo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-simple-demo.azurewebsites.net', 'localhost', '127.0.0.1']
 
 
 # Application definition
