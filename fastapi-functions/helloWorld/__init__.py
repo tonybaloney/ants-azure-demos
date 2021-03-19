99c345fc5109fa170f4e8c119c64acd0eda80204
Anthony Shaw <anthony.p.shaw@gmail.com>
2021-03-19 15:10:41 +1100

    Stuff a bit more in there

--- fastapi-functions/helloWorld/__init__.py
+++ fastapi-functions/helloWorld/__init__.py
@@ -1,13 +1,21 @@
 import fastapi
 from functions_helpers import as_function
-
+import mimesis
 
 app = fastapi.FastAPI()
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int):
-    return {"item_id": item_id}
+@app.get("/users/{user_id}")
+def read_item(user_id: int):
+    fake_user = mimesis.Person()
+    return {
+        "user_id": user_id,
+        "username": fake_user.username(),
+        "fullname": fake_user.full_name(),
+        "age": fake_user.age(),
+        "firstname": fake_user.first_name(),
+        "lastname": fake_user.last_name(),
+    }
 
 
 main = as_function(read_item)
--- fastapi-functions/tests/test_basic.py
+++ fastapi-functions/tests/test_basic.py
@@ -1,11 +1,12 @@
 import azure.functions as func
 import helloWorld
+import json
 
 
 def test_basic():
     req = func.HttpRequest(
-        method="GET", url="items/2", body=b"", route_params={"item_id": "2"}
+        method="GET", url="users/2", body=b"", route_params={"user_id": "2"}
     )
     result = helloWorld.main(req)
     assert result.status_code == 200
-    assert result.get_body().decode() == '{"item_id": "2"}'
+    assert json.loads(result.get_body().decode())["user_id"] == "2"
