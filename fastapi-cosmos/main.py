a8284e32d203b8f813f9b4e96c2e5a9d6f2e776e
Anthony Shaw <anthony.p.shaw@gmail.com>
2021-05-06 07:51:09 +1000

    Better error handling

--- fastapi-cosmos/main.py
+++ fastapi-cosmos/main.py
@@ -7,13 +7,9 @@ import azure.cosmos.exceptions as exceptions
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
+USERPROFILE_CONTAINER = "userprofile"
 
-def get_database() -> DatabaseProxy:
-    client = CosmosClient(
-        url=os.getenv("COSMOS_URL"), credential=os.getenv("COSMOS_KEY")
-    )
-
-    return client.create_database_if_not_exists(os.getenv("COSMOS_DATABASE", "fastapi"))
+# Models
 
 
 class User(BaseModel):
@@ -23,59 +19,87 @@ class User(BaseModel):
     disabled: Optional[bool] = None
 
 
+# Cosmos DB Connection
+
+
+def get_database() -> DatabaseProxy:
+    client = CosmosClient(
+        url=os.getenv("COSMOS_URL"), credential=os.getenv("COSMOS_KEY")
+    )
+
+    return client.create_database_if_not_exists(os.getenv("COSMOS_DATABASE", "fastapi"))
+
+
+# Helper functions for reading/writing from Cosmos.
+
+
 def get_user(database: DatabaseProxy, username: str):
     try:
-        result = database.get_container_client(container="userprofile").read_item(
-            item=username, partition_key=username
-        )
+        result = database.get_container_client(
+            container=USERPROFILE_CONTAINER
+        ).read_item(item=username, partition_key=username)
+
+        if not result:
+            return None
+        user = User(**result)
+        return user
     except exceptions.CosmosResourceNotFoundError:
         raise HTTPException(status_code=404, detail="User not found")
-    if not result:
-        return None
-    user = User(**result)
-    return user
+    except exceptions.CosmosHttpResponseError as cxe:
+        raise HTTPException(
+            status_code=400, detail="Bad request {0}".format(cxe.http_error_message)
+        )
 
 
 def get_users(database: DatabaseProxy):
     try:
-        result = database.get_container_client(container="userprofile").read_all_items()
+        result = database.get_container_client(
+            container=USERPROFILE_CONTAINER
+        ).read_all_items()
+        users = [User(**item) for item in result]
+        return users
     except exceptions.CosmosResourceNotFoundError:
-        raise HTTPException(status_code=404, detail="User not found")
-    if not result:
-        return None
-    users = [User(**item) for item in result]
-    return users
+        database.create_container(USERPROFILE_CONTAINER)
+        return []
+    except exceptions.CosmosHttpResponseError as cxe:
+        raise HTTPException(
+            status_code=400, detail="Bad request {0}".format(cxe.http_error_message)
+        )
 
 
 def _create_user(database: DatabaseProxy, user: User):
     try:
         new_user = user.dict()
         new_user["id"] = user.username
-        database.get_container_client(container="userprofile").upsert_item()
-    except exceptions.CosmosHttpResponseError:
-        raise HTTPException(status_code=401, detail="Invalid request")
-    return
+        database.get_container_client(container=USERPROFILE_CONTAINER).upsert_item()
+        return
+    except exceptions.CosmosHttpResponseError as cxe:
+        raise HTTPException(
+            status_code=400, detail="Bad request {0}".format(cxe.http_error_message)
+        )
 
 
 # FastAPI specific code
 app = FastAPI()
 
+# API Routes
+
 
 @app.get("/users/{username}", response_model=User)
-def read_user(username: str):
+async def read_user(username: str):
     database = get_database()
     user = get_user(database=database, username=username)
     return user
 
 
 @app.get("/users/", response_model=List[User])
-def read_users():
+async def read_users():
     database = get_database()
     users = get_users(database=database)
     return users
 
 
 @app.post("/users/")
-def create_user(user: User):
+async def create_user(user: User):
     database = get_database()
     return _create_user(database=database, user=user)
