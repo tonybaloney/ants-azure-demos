3ea4e1b079d70bc0246c69ae95d699a99cc4cd05
Anthony Shaw <anthony.p.shaw@gmail.com>
2021-05-05 18:45:21 +1000

    List users as well

--- fastapi-cosmos/main.py
+++ fastapi-cosmos/main.py
@@ -1,9 +1,10 @@
-from typing import Optional
+from typing import Optional, List
 import os
 
 from azure.cosmos import CosmosClient
 from azure.cosmos.database import DatabaseProxy
-from fastapi import FastAPI
+import azure.cosmos.exceptions as exceptions
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 
@@ -12,7 +13,7 @@ def get_database() -> DatabaseProxy:
         url=os.getenv("COSMOS_URL"), credential=os.getenv("COSMOS_KEY")
     )
 
-    return client.create_database_if_not_exists(os.getenv("COSMOS_DATABASE"))
+    return client.create_database_if_not_exists(os.getenv("COSMOS_DATABASE", "fastapi"))
 
 
 class User(BaseModel):
@@ -23,13 +24,27 @@ class User(BaseModel):
 
 
 def get_user(database: DatabaseProxy, username: str):
-    result = database.get_container_client(container="userprofile").read_item(
-        item={"username": username}
-    )
+    try:
+        result = database.get_container_client(container="userprofile").read_item(
+            item=username, partition_key=username
+        )
+    except exceptions.CosmosResourceNotFoundError:
+        raise HTTPException(status_code=404, detail="User not found")
     if not result:
         return None
     user = User(**result)
     return user
+
+
+def get_users(database: DatabaseProxy):
+    try:
+        result = database.get_container_client(container="userprofile").read_all_items()
+    except exceptions.CosmosResourceNotFoundError:
+        raise HTTPException(status_code=404, detail="User not found")
+    if not result:
+        return None
+    users = [User(**item) for item in result]
+    return users
 
 
 # FastAPI specific code
@@ -41,3 +56,10 @@ def read_user(username: str):
     database = get_database()
     user = get_user(database=database, username=username)
     return user
+
+
+@app.get("/users/", response_model=List[User])
+def read_users():
+    database = get_database()
+    users = get_users(database=database)
+    return users
