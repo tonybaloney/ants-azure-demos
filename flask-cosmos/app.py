577dea22e552baa5141daa4b4657e5cbe06c6496
Anthony Shaw <anthony.p.shaw@gmail.com>
2021-05-06 12:40:26 +1000

    Initial application prototype

---
+++ flask-cosmos/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+import os
+
+app = Flask(__name__)
+app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+mongo = PyMongo(app)
+
+
+@app.route("/")
+def home_page():
+    online_users = mongo.cx["django_cosmos"].users.find({"online": True})
+    return render_template("index.html",
+        users=online_users)
