5cc5755205e0aaac867334211124eb7c2beb74a0
Anthony Shaw <anthony.p.shaw@gmail.com>
2021-06-03 11:04:50 +1000

    update default URL path

--- django-web-app/antdemo/urls.py
+++ django-web-app/antdemo/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("/", include("demo.urls")),
+    path("", include("demo.urls")),
     path("admin/", admin.site.urls),
 ]
