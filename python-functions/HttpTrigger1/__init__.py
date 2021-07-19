7f10e557f595ddec43aed9fa52c9d0e49809ee8c
Anthony Shaw <anthony.p.shaw@gmail.com>
2021-07-19 12:38:50 +1000

    that shouldn't be there...

--- python-functions/HttpTrigger1/__init__.py
+++ python-functions/HttpTrigger1/__init__.py
@@ -2,10 +2,6 @@ import logging
 
 import azure.functions as func
 
-import yaml
-
-yaml.load("bad!!!")
-
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info("Python HTTP trigger function processed a request.")
